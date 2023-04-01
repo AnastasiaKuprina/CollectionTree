f634cdded8fd842095cf105c03cbc3afb09c4750
Anastasia Kuprina <ApplicationMath@gmail.com>
2023-04-01 19:12:34 +0500

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -11,16 +12,20 @@ public class Main {
         people.add(new Person("Кузьма", "Петров Водкин кк", 52));
         people.add(new Person("Кузьма", "Петров Водкин Алексеев", 41));
         people.add(new Person("Чарльз", "Филип Артур Джордж Маунтбеттен Виндзор", 74));
-        people.add(new Person("Морена", "Сильва де Ваз Сетта Баккарин", 43));
-        people.add(new Person("Билли", "Айлиш Оконнел", 20));
+        people.add(new Person("Морена", "Сильва де Ваз Сетта Баккарин", 12));
+        people.add(new Person("Билли", "Айлиш Оконнел", 17));
 
+        Comparator<Person> PersonsSurnameAgeComparator = (o1, o2) -> {
+            if (o1.getSurnameSet().size() == o2.getSurnameSet().size()
+                    || (!(o1.getSurnameSet().size() < 4)
+                    && o2.getSurnameSet().size() >= 4))
+                return Integer.compare(o1.getAge(), o2.getAge());
+            else return Integer.compare(o1.getSurnameSet().size(), o2.getSurnameSet().size());
+        };
 
-        // Задача №1. Сортировка по количеству слов в фамилии
-        Collections.sort(people, new PersonsSurnameAgeComparator(0));// Задача 1
-        System.out.printf("\nЗадача №1 :%s.\n", people.toString().replaceAll("\\[|\\]", ""));
+        // Сортировка по количеству слов в фамилии
+        Collections.sort(people, PersonsSurnameAgeComparator);
+        System.out.printf(people.toString().replaceAll("\\[|\\]", ""));
 
-        // Задача №2. Сортировка с учётом макс количества слов
-        Collections.sort(people, new PersonsSurnameAgeComparator(4));// Задача 2
-        System.out.printf("\nЗадача №2 :%s.\n", people.toString().replaceAll("\\[|\\]", ""));
     }
 }
